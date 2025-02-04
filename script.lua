6f02c3062364660ce9d4f3f329f3b434f9719401
ThatPatrick <patrick.romanenko@mail.ru>
2025-02-05 01:43:17 +0600

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,5 @@
-print("Hello")
+repeat task.wait() until game:IsLoaded()
+
+queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/ThatPatrick/UniversalScript/refs/heads/main/script.lua"))()')
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ThatPatrick/UniversalScript/refs/heads/main/"..game.GameId..".lua"))()
